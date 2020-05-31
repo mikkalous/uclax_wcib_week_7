86c4280fab63e5c7188f5d24dda9108f423dd673
mmm <63331878+mikkalous@users.noreply.github.com>
2020-05-30 22:00:08 -0700

    first load

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+jQuery(function() {
+
+	let my_switch = false;
+
+	$('#my_button').click(function () {
+
+		if ( my_switch == false ) {
+			my_switch = true;
+
+		$(this).html('Yes it will!')
+						.removeClass('btn-secondary')
+						  .addClass('btn-success');
+
+		$("#my_name, #my_text")
+						.removeClass("sprout")
+						 .addClass("bloom");
+
+		$('#my_flower').attr('src', 'images/before.png');
+
+	} else {
+		my_switch = false;
+
+		$(this).html('Will it bloom?')
+						.removeClass('btn-success')
+							.addClass('btn-secondary');
+
+		$("#my_name, #my_text")
+						.removeClass("bloom")
+		 				 	.addClass("sprout");
+
+		$('#my_flower').attr('src', 'images/after.png');
+
+	}
+
+	});
+
+});
